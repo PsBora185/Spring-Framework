ae559d0151ead078580da55734b941abd9107d0a
pranav <pranavsinghbora@gmail.com>
2026-02-25 13:51:14 +0530

    Tight coupling

---
+++ StrategyDesignPattern/src/tight/CardPay.java
@@ -0,0 +1,7 @@
+package src.tight;
+
+public class CardPay {
+    public void pay(Double amt){
+        System.out.println("Pay "+amt);
+    }
+}
---
+++ StrategyDesignPattern/src/tight/UpiPay.java
@@ -0,0 +1,7 @@
+package src.tight;
+
+public class UpiPay {
+    public void pay(Double amt){
+        System.out.println("Pay "+amt);
+    }
+}
